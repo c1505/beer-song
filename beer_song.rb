44d383ae634bae9f8267ed951573582117a7a028
c1505 <coreym247@gmail.com>
2016-07-30 07:34:01 -0400

    update

--- beer_song.rb
+++ beer_song.rb
@@ -1,9 +1,38 @@
+require 'pry'
 class BeerSong
 
   def verse(line)
-    line2 = line - 1 
-    "#{line} bottles of beer on the wall, #{line} bottles of beer.\n" \
-    "Take one down and pass it around, #{line2} bottles of beer on the wall.\n"
+    case line
+    when 2
+      line2 = line - 1 
+      "#{line} bottles of beer on the wall, #{line} bottles of beer.\n" \
+      "Take one down and pass it around, #{line2} bottle of beer on the wall.\n"
+    when 1
+      "#{line} bottle of beer on the wall, #{line} bottle of beer.\n" \
+      "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\n" \
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    else
+      line2 = line - 1 
+      "#{line} bottles of beer on the wall, #{line} bottles of beer.\n" \
+      "Take one down and pass it around, #{line2} bottles of beer on the wall.\n"
+    end
+  end
+
+  def verses(line1, line2)
+    verse(line1)
+  end
+
+  def lyrics
+    line = 99
+    str = ""
+    while line > 1
+      str = str + verse(line)
+      line -= 1
+    end
+    str
+    binding.pry
   end
 end
 
--- beer_song_test.rb
+++ beer_song_test.rb
@@ -33,14 +33,14 @@ class BeerSongTest < Minitest::Test
   end
 
   def test_verse_0
-
+    
     expected = "No more bottles of beer on the wall, no more bottles of beer.\n" \
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     assert_equal expected, BeerSong.new.verse(0)
   end
 
   def test_a_couple_verses
-    skip
+    
     expected = "99 bottles of beer on the wall, 99 bottles of beer.\n" \
       "Take one down and pass it around, 98 bottles of beer on the wall.\n" \
       "\n" \
@@ -50,7 +50,7 @@ class BeerSongTest < Minitest::Test
   end
 
   def test_a_few_verses
-    skip
+    
     expected = "2 bottles of beer on the wall, 2 bottles of beer.\n" \
       "Take one down and pass it around, 1 bottle of beer on the wall.\n" \
       "\n" \
@@ -63,7 +63,7 @@ class BeerSongTest < Minitest::Test
   end
 
   def test_the_whole_song # rubocop:disable Metrics/MethodLength
-    skip
+    
     expected = <<-SONG
 99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
